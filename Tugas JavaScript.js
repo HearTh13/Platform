c719a13d0a70bbf4d08d816b4055a5c673942f03
HearTh13 <gun123sokero@gmail.com>
2024-02-28 14:54:32 +0700

    modified:   README.md modified:   Tugas JavaScript.html modified:   Tugas JavaScript.js

--- Tugas JavaScript.js
+++ Tugas JavaScript.js
@@ -1,13 +1,30 @@
 document.getElementById("btn").addEventListener("click",function(e){
     e.preventDefault();
     let jumlah = document.getElementById("jumlahPilihan").value;
-    let text;
+    let text = '<form>';
     console.log(jumlah);
 
     for (let i = 0; i <jumlah; i++) {
         text += '<label>Pilihan</label>';
         text += '<input type="text" required>';
     }
-    text += '<br><button>Submit</button>';
+    text += '<br><button id="btn2">Submit</button>';
+    text += '</form>';
+    text += '<div id="form2"></div>';
     document.getElementById("form1").innerHTML = text;
+});
+
+document.getElementById("btn2").addEventListener("click",function(e) {
+    e.preventDefault();
+    let jumlah = document.getElementById("jumlahPilihan").value;
+    let text = '<form>';
+    console.log(jumlah);
+
+    for (let i = 0; i <jumlah; i++) {
+        text += '<input type="dropdown">';
+        text += '<label>Pilihan</label>';
+    }
+    text += '<br><button>Submit</button>';
+    text += '</form>';
+    document.getElementById("form2").innerHTML = text;
 });
