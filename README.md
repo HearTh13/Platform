d2cc44290c59b0623e731ec35c84f6659fb7f241
HearTh13 <gun123sokero@gmail.com>
2024-03-05 14:08:00 +0700

    Terdapat beberapa Perubahan nama File: - Tugas html.html dirubah menjadi Tugas 2.html dan Tugas 3a.html - Tugas css.css dirubah menjadi Tugas 3b.css - Tugas JavaScript.html dirubah menjadi Tugas 4a.html - Tugas JavaScript.css dirubah menjadi Tugas 4b.css - Tugas JavaScript.js dirubah menjadi Tugas 4c.js

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Platform
-Tugas tugas ada disini hehe 
 
-1. Tugas html.html untuk tugas html 
-2. Tugas html.html dan css.css untuk tugas css 
-3. Tugas Tugas JavaScript.js, Tugas JavaScript.html dan Tugas JavaScript.css untuk tugas javascript
+Terdapat beberapa Perubahan nama File:
+- Tugas html.html dirubah menjadi Tugas 2.html dan Tugas 3a.html
+- Tugas css.css dirubah menjadi Tugas 3b.css
+- Tugas JavaScript.html dirubah menjadi Tugas 4a.html
+- Tugas JavaScript.css dirubah menjadi Tugas 4b.css
+- Tugas JavaScript.js dirubah menjadi Tugas 4c.js
+
+1. Folder Tugas 2 untuk tugas 2
+2. Folder Tugas 3 untuk tugas 3 
+3. Folder Tugas 4 untuk tugas 4
--- Tugas 4/Tugas 4c.js
+++ Tugas 4/Tugas 4c.js
@@ -1,8 +1,9 @@
 var tampilanInputs;
 var data = [];
+var radio = [];
 var pilihan;
 
-document.getElementById('btn').addEventListener('click', function(e) {
+document.getElementById('btn').addEventListener('click', function (e) {
     textInput();
 });
 
@@ -17,15 +18,16 @@ function textInput() {
     for (var i = 1; i <= jumlah; i++) {
         var label = document.createElement("label");
         label.innerHTML = "Pilihan " + i + ":";
-        
-        input[i] = document.createElement("input");
-        input.type = "text";
-        input.name = "teksTampilan" + i;
+
+        input[i - 1] = document.createElement("input");
+        input[i - 1].type = "text";
+        input[i - 1].name = "teksTampilan" + i;
 
         tampilanInputs.appendChild(label);
-        tampilanInputs.appendChild(input[i]);
+        tampilanInputs.appendChild(input[i - 1]);
         tampilanInputs.appendChild(document.createElement("br"));
     }
+
     var button = document.createElement("button");
     button.type = "button";
     button.id = "buttonOK";
@@ -35,9 +37,9 @@ function textInput() {
     tampilanInputs.appendChild(document.createElement("br"));
     tampilanInputs.appendChild(document.createElement("br"));
 
-    document.getElementById("buttonOK").addEventListener("click", function(e){
+    document.getElementById("buttonOK").addEventListener("click", function (e) {
         for (var i = 1; i <= jumlah; i++) {
-            data[i] = input[i].value;
+            data[i - 1] = input[i - 1].value;
         }
         textRadio();
     });
@@ -51,24 +53,33 @@ function textRadio() {
 
     for (var i = 1; i <= jumlah; i++) {
         var label = document.createElement("label");
-        label.innerHTML ='<input type = "Radio" name = "teksTampilan" id = "Radio'+i+'">' + " Pilihan " + i;
-        label.setAttribute("for", "pilihan" + i);
-        
+        var radioInput = document.createElement("input");
+        radioInput.type = "radio";
+        radioInput.name = "teksTampilan";
+        radioInput.id = "Radio" + i;
+
+        label.innerHTML += " Pilihan " + i;
+
         tampilanInputs.appendChild(label);
+        tampilanInputs.appendChild(radioInput);
+
+        
     }
 
-    for (i = 1; i <= jumlah.length; i++) {
-        var radio = document.getElementById("Radio"+i);
-        radio.addEventListener("click", function(){
-            var radios = document.getElementsByName(this.name);
-            
-            radios.forEach(function (radio) {
-                radio.checked = false;
+    for (i = 1; i <= jumlah; i++) {
+        radio[i - 1] = document.getElementById("Radio" + i);
+        radio[i - 1].addEventListener("click", function (e) {
+            var radios = document.getElementsByName("teksTampilan");
+
+            radios.forEach(function (x) {
+                x.checked = false;
             });
-            
+
             this.checked = true;
         });
     }
+
+    console.log(radio);
 
     var button = document.createElement("button");
     button.name = "buttonLast";
@@ -78,10 +89,21 @@ function textRadio() {
     tampilanInputs.appendChild(button);
     tampilanInputs.appendChild(document.createElement("br"));
 
-    document.getElementById("buttonOK").addEventListener("click", function(e){
+    var div = document.createElement("div");
+    div.id = "Last";
+    document.getElementById("buttonLast").addEventListener("click", function (e) {
+        pilihan = '';
+        console.log(e);
         for (var i = 1; i <= jumlah; i++) {
-            data[i] = input[i].value;
+            if (radio[i - 1].checked) {
+                pilihan = data[i - 1];
+                console.log(pilihan);
+            }
         }
-        textRadio();
+        tampilkanData();
     });
+}
+
+function tampilkanData() {
+    // Implement your logic here
 }
