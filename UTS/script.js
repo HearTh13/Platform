9b71ced38e1146cad09579ca0b6fe35ff006d214
HearTh13 <gun123sokero@gmail.com>
2024-03-26 15:30:29 +0700

    modified:   Tugas 4/Tugas 4a.html modified:   UTS/beranda.html modified:   UTS/kisahku.html modified:   UTS/script.js modified:   UTS/style.css modified:   UTS/tentang.html modified:   UTS/tugas.html

---
+++ UTS/script.js
@@ -0,0 +1,128 @@
+var tampilanInputs;
+var nama;
+var data = [];
+var radio = [];
+var jumlah;
+var pilihan;
+
+document.getElementById('btn').addEventListener('click', function () {
+  jumlah = document.getElementById('jumlah').value;
+  nama = document.getElementById('nama').value;
+  if (nama !== '' && jumlah !== '' && nama !== null && jumlah !== null) {
+    this.remove();
+    textInput();
+  }
+});
+
+function textInput() {
+  tampilanInputs = document.getElementById('tampilanInputs1');
+
+  tampilanInputs.innerHTML = '';
+
+  var input = [];
+
+  for (var i = 1; i <= jumlah; i++) {
+    var label = document.createElement('label');
+    label.innerHTML = 'Pilihan ' + i + ':';
+
+    input[i - 1] = document.createElement('input');
+    input[i - 1].type = 'text';
+    input[i - 1].name = 'teksTampilan' + i;
+
+    tampilanInputs.appendChild(label);
+    tampilanInputs.appendChild(input[i - 1]);
+    tampilanInputs.appendChild(document.createElement('br'));
+  }
+
+  var button = document.createElement('button');
+  button.type = 'button';
+  button.id = 'buttonOK';
+  button.innerHTML = 'OK';
+
+  tampilanInputs.appendChild(button);
+
+  document.getElementById('buttonOK').addEventListener('click', function () {
+    var check = true;
+    for (var i = 0; i < jumlah; i++) {
+      data[i] = input[i].value;
+      if (data[i] == '' || data[i] == null) {
+        check = false;
+      }
+      console.log(data[i]);
+      console.log(check);
+    }
+    if (check) {
+      button.remove();
+      textRadio();
+    }
+  });
+}
+
+function textRadio() {
+  jumlah = document.getElementById('jumlah').value;
+  tampilanInputs = document.getElementById('tampilanInputs2');
+
+  console.log(data);
+
+  for (var i = 1; i <= jumlah; i++) {
+    var label = document.createElement('label');
+    label.innerHTML = data[i - 1];
+
+    radio[i - 1] = document.createElement('input');
+    radio[i - 1].type = 'radio';
+    radio[i - 1].name = 'teksTampilan';
+    radio[i - 1].id = 'Radio' + i;
+
+    tampilanInputs.appendChild(radio[i - 1]);
+    tampilanInputs.appendChild(label);
+
+    radio[i - 1].addEventListener('click', function () {
+      for (j = 0; j < jumlah; j++) {
+        radio[j].checked = false;
+      }
+      this.checked = true;
+    });
+  }
+
+  console.log(radio);
+
+  var button = document.createElement('button');
+  button.name = 'buttonLast';
+  button.textContent = 'OK';
+  button.id = 'buttonLast';
+
+  tampilanInputs.appendChild(button);
+
+  document.getElementById('buttonLast').addEventListener('click', function () {
+    console.log(radio[0].checked);
+    console.log(radio[1].checked);
+    var check = false;
+    for (var i = 0; i < jumlah; i++) {
+      if (radio[i].checked) {
+        check = true;
+        pilihan = data[i];
+        console.log(pilihan);
+      }
+    }
+    if (check) {
+      tampilkanData();
+      button.remove();
+    }
+  });
+}
+
+function tampilkanData() {
+  tampilanInputs = document.getElementById('tampilanInputs3');
+  console.log(jumlah);
+  var tampil = document.createElement('div');
+  tampil.innerHTML = '<p>Hallo, nama saya ' + nama + ', saya mempunyai sejumlah ' + jumlah + ' pilihan yaitu: ';
+  for (var i = 0; i < jumlah; i++) {
+    if (i == jumlah - 1) {
+      tampil.innerHTML += ' dan ' + data[i];
+    } else {
+      tampil.innerHTML += data[i] + ', ';
+    }
+  }
+  tampil.innerHTML += ' dan saya memilih ' + pilihan + '</p>';
+  tampilanInputs.appendChild(tampil);
+}
