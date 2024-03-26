e2b45a53b819b0df12aedcd3e269f895a5a1112f
HearTh13 <gun123sokero@gmail.com>
2024-03-26 17:08:58 +0700

    modified:   UTS/script.js modified:   UTS/style.css modified:   UTS/tentang.html modified:   UTS/tugas.html

--- UTS/script.js
+++ UTS/script.js
@@ -1,6 +1,7 @@
 var tampilanInputs;
 var fnama;
 var lnama;
+var email;
 var data = [];
 var radio = [];
 var jumlah;
@@ -10,6 +11,7 @@ document.getElementById('btn').addEventListener('click', function () {
   jumlah = document.getElementById('jumlah').value;
   fnama = document.getElementById('fnama').value;
   lnama = document.getElementById('lnama').value;
+  email = document.getElementById('email').value;
   if (fnama !== '' && fnama !== '' && jumlah !== '' && fnama !== null && fnama !== null && jumlah !== null) {
     this.remove();
     textInput();
@@ -26,24 +28,29 @@ function textInput() {
   for (var i = 1; i <= jumlah; i++) {
     var label = document.createElement('label');
     label.className = 'text-light';
-    label.innerHTML = 'Pilihan ' + i + ':';
+    label.innerHTML = 'Hobby ' + i + ':';
 
     input[i - 1] = document.createElement('input');
     input[i - 1].type = 'text';
     input[i - 1].name = 'teksTampilan' + i;
-    input[i - 1].className = 'opacity-05';
+    input[i - 1].className = 'form-control opacity-05';
 
     tampilanInputs.appendChild(label);
     tampilanInputs.appendChild(input[i - 1]);
     tampilanInputs.appendChild(document.createElement('br'));
   }
 
+  var konten = document.createElement('div');
+  konten.className = 'text-center';
+
   var button = document.createElement('button');
   button.type = 'button';
   button.id = 'buttonOK';
-  button.innerHTML = 'OK';
+  button.className = 'btn btn-dark';
+  button.innerHTML = 'Submit';
 
-  tampilanInputs.appendChild(button);
+  konten.appendChild(button);
+  tampilanInputs.appendChild(konten);
 
   document.getElementById('buttonOK').addEventListener('click', function () {
     var check = true;
@@ -71,14 +78,18 @@ function textRadio() {
   for (var i = 1; i <= jumlah; i++) {
     var label = document.createElement('label');
     label.innerHTML = data[i - 1];
+    label.className = 'text-light pad-3';
 
     radio[i - 1] = document.createElement('input');
     radio[i - 1].type = 'radio';
     radio[i - 1].name = 'teksTampilan';
     radio[i - 1].id = 'Radio' + i;
+    radio[i - 1].className = 'form-check-input';
 
     tampilanInputs.appendChild(radio[i - 1]);
     tampilanInputs.appendChild(label);
+    tampilanInputs.appendChild(document.createElement('br'));
+    tampilanInputs.appendChild(document.createElement('br'));
 
     radio[i - 1].addEventListener('click', function () {
       for (j = 0; j < jumlah; j++) {
@@ -88,14 +99,19 @@ function textRadio() {
     });
   }
 
-  console.log(radio);
+  tampilanInputs.appendChild(document.createElement('br'));
+
+  var konten = document.createElement('div');
+  konten.className = 'text-center';
 
   var button = document.createElement('button');
   button.name = 'buttonLast';
-  button.textContent = 'OK';
+  button.textContent = 'Submit';
   button.id = 'buttonLast';
+  button.className = 'btn btn-dark';
 
-  tampilanInputs.appendChild(button);
+  konten.appendChild(button);
+  tampilanInputs.appendChild(konten);
 
   document.getElementById('buttonLast').addEventListener('click', function () {
     console.log(radio[0].checked);
@@ -119,7 +135,7 @@ function tampilkanData() {
   tampilanInputs = document.getElementById('tampilanInputs3');
   console.log(jumlah);
   var tampil = document.createElement('div');
-  tampil.innerHTML = '<p>Hallo, nama saya ' + nama + ', saya mempunyai sejumlah ' + jumlah + ' pilihan yaitu: ';
+  tampil.innerHTML = '<p>Hallo, nama saya ' + fnama + ' ' + lnama + ' dengan e-mail' + email + ', saya mempunyai sejumlah ' + jumlah + ' hobby pilihan yaitu: ';
   for (var i = 0; i < jumlah; i++) {
     if (i == jumlah - 1) {
       tampil.innerHTML += ' dan ' + data[i];
