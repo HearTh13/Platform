7c7216a63c7479d9c859857b4e4acf5a84a95bce
HearTh13 <gun123sokero@gmail.com>
2024-04-03 09:03:31 +0700

    new file:   Tugas 5/test.php modified:   UTS/tugas.html

---
+++ Tugas 5/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>
+      Belajar PHP
+    </title>
+  </head>
+  <body>
+    <h1>Selamat Datang <?php echo "Achmad"; ?></h1>
+    <p><?php echo "Ini adalah Paragraph"; ?></p>
+  </body>
+</html>
