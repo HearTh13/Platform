8faebc07292ac7efc28bdf9a216abbd694f6c304
HearTh13 <gun123sokero@gmail.com>
2024-04-17 08:20:16 +0700

    modified:   Tugas 5/test.php

--- Tugas 5/test.php
+++ Tugas 5/test.php
@@ -40,5 +40,36 @@ $y = 20;
       print($y);
       $x++;
     } while($x < $y) ?>
+
+    <table border="1" cellpadding="10" cellpadding="0">
+     <?php 
+      for($i=1; $i <= 3; $i++){
+        echo "<tr>";
+        for($j=1; $j <= 5; $j++){ 
+          echo"<td>$i,$j</td>";
+        };
+        echo "</tr>";
+      }
+     ?>
+     <?php for($i=1; $i <= 3; $i++): ?>
+      <tr>
+        <?php for($j=1; $j <= 5; $j++){ ?>
+          <td><?php echo "$i,$j"; ?></td>
+        <?php } ?>
+      </tr>
+     <?php endfor; ?>
+    </table>
+    <br>
+    <?php 
+      $m = 10;
+      if($m < 20){
+        echo "benar";
+      } else if($m == 20){
+        echo "bingo!"
+      } else{
+        echo "salah";
+      }
+    ?>
+
   </body>
 </html>
