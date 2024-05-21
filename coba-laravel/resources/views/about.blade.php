c2bad49eb3ca06b4c9b2982319713544dcf7098d
HearTh13 <gun123sokero@gmail.com>
2024-05-21 08:30:42 +0700

    modified:   coba-laravel/resources/views/about.blade.php modified:   coba-laravel/resources/views/home.blade.php modified:   coba-laravel/resources/views/layouts/main.blade.php modified:   coba-laravel/resources/views/partials/navbar.blade.php modified:   coba-laravel/routes/web.php

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.main')
 
 @section('content')
-  <title>Halaman | About</title>
   <img src="https://i.pinimg.com/736x/0f/60/76/0f607674cc6dd7a224857a690c59b807.jpg" alt="My Face" width="200" class="rounded-circle img-thumbnail" />
   <h1 class="text-light fs-5">{{ $Nama }}</h1>
   <p class="con text-light">{{$NIM}}</p>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -3,20 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "Nama" => "Achmad Herlambang Prasetyo",
         "NIM" => "225314018"
     ]);
 });
 
 Route::get('/login', function () {
-    return view('login');
+    return view('login', [
+        "title" => "Login"
+    ]);
 });
 
 Route::get('/register', function () {
-    return view('register');
+    return view('register', [
+        "title" => "Register"
+    ]);
 });
