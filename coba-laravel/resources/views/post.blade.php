cea33f3e2c9976524007ceb4a85ed0de918480e2
HearTh13 <gun123sokero@gmail.com>
2024-05-22 15:51:33 +0700

    new file:   coba-laravel/app/Http/Controllers/PostController.php modified:   coba-laravel/app/Models/Post.php modified:   coba-laravel/resources/views/post.blade.php modified:   coba-laravel/routes/web.php

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <article class="mb-5">
+    <article class="mb-5 mt-5">
         <h1 class="text-dark"> {{$post["Judul"]}} </h1>
         <h2 class="text-dark">By: {{$post["Author"]}} </h2>
         <p class="text-dark"> {{$post["Body"]}} </p>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 Route::get('/', function () {
     return view('home', [
@@ -28,51 +31,5 @@ Route::get('/register', function () {
     ]);
 });
 
-Route::get('/posts', function () {
-    $blog = [
-        [
-            "Judul" => "Satu",
-            "slug" => "judul-post-pertama",
-            "Author" => "Tyo",
-            "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolore eos error enim repellendus dignissimos ex officia cumque possimus numquam praesentium dicta, animi voluptate excepturi quisquam commodi, assumenda recusandae necessitatibus nesciunt debitis fuga deleniti similique molestiae iusto. Consequuntur praesentium impedit odit quaerat necessitatibus deserunt mollitia odio similique aliquam repudiandae ad nam, est repellendus pariatur ea cupiditate. Reiciendis explicabo repellendus iste. Sunt, labore fugiat, inventore ex vero nostrum ad mollitia repudiandae accusantium et aliquid nisi, facilis explicabo vel veritatis tenetur! Et reprehenderit, fugiat saepe ad, recusandae ipsum officia incidunt, sed consectetur excepturi perspiciatis voluptatem commodi. Vel dolores dolorum cumque ipsum itaque."
-        ],
-        [
-            "Judul" => "Dua",
-            "slug" => "judul-post-kedua",
-            "Author" => "Achmad",
-            "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eius odio quas! Corporis quidem, velit enim perspiciatis reiciendis necessitatibus hic nemo? Itaque suscipit maxime voluptatibus explicabo, quasi hic. Labore quo doloribus aspernatur odio. Et, molestiae neque deserunt dolores consequuntur quam officia libero velit quos impedit possimus nostrum, ratione, quaerat ea omnis fuga corrupti sint? Vero explicabo adipisci ex! Totam, minima!"
-        ],
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog
-    ]);
-});
-
-Route::get('posts/{slug}', function($slug){
-    $blog = [
-        [
-            "Judul" => "Satu",
-            "slug" => "judul-post-pertama",
-            "Author" => "Tyo",
-            "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolore eos error enim repellendus dignissimos ex officia cumque possimus numquam praesentium dicta, animi voluptate excepturi quisquam commodi, assumenda recusandae necessitatibus nesciunt debitis fuga deleniti similique molestiae iusto. Consequuntur praesentium impedit odit quaerat necessitatibus deserunt mollitia odio similique aliquam repudiandae ad nam, est repellendus pariatur ea cupiditate. Reiciendis explicabo repellendus iste. Sunt, labore fugiat, inventore ex vero nostrum ad mollitia repudiandae accusantium et aliquid nisi, facilis explicabo vel veritatis tenetur! Et reprehenderit, fugiat saepe ad, recusandae ipsum officia incidunt, sed consectetur excepturi perspiciatis voluptatem commodi. Vel dolores dolorum cumque ipsum itaque."
-        ],
-        [
-            "Judul" => "Dua",
-            "slug" => "judul-post-kedua",
-            "Author" => "Achmad",
-            "Body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eius odio quas! Corporis quidem, velit enim perspiciatis reiciendis necessitatibus hic nemo? Itaque suscipit maxime voluptatibus explicabo, quasi hic. Labore quo doloribus aspernatur odio. Et, molestiae neque deserunt dolores consequuntur quam officia libero velit quos impedit possimus nostrum, ratione, quaerat ea omnis fuga corrupti sint? Vero explicabo adipisci ex! Totam, minima!"
-        ],
-    ];
-    $new_post = [];
-    foreach ($blog as $post){
-        if ($post["slug"] === $slug){
-            $new_post = $post;
-        }
-    }
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
