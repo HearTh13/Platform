f88f63fd26d96a87ed76d247b24add86ca40b974
HearTh13 <gun123sokero@gmail.com>
2024-05-24 09:07:14 +0700

    deleted:    Platform-Project/public/project/login.html renamed:    Platform-Project/public/project/index.html -> Platform-Project/resources/views/index.blade.php new file:   Platform-Project/resources/views/login.blade.php modified:   Platform-Project/routes/web.php

--- Platform-Project/resources/views/index.blade.php
+++ Platform-Project/resources/views/index.blade.php
@@ -1,38 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Netflix</title>
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-            crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="project/style.css" />
     </head>
     <body>
-
         <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-            crossorigin="anonymous"></script>
+            crossorigin="anonymous"
+        ></script>
 
         <nav class="navbar navbar-expand-lg bg-transparent mt-5">
             <div class="container-fluid">
                 <a class="navbar-brand text-#dc3545" href="#">NETFLIX</a>
-                <button class="navbar-toggler" type="button"
+                <button
+                    class="navbar-toggler"
+                    type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active text-white"
-                                aria-current="page" href="#">Home</a>
+                            <a
+                                class="nav-link active text-white"
+                                aria-current="page"
+                                href="#"
+                                >Home</a
+                            >
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="#">Horror</a>
@@ -41,63 +50,95 @@
                             <a class="nav-link text-white" href="#">Sci-Fi</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-white"
-                                href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
+                            <a
+                                class="nav-link dropdown-toggle text-white"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
                                 More
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item"
-                                        href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another
-                                        action</a></li>
-                                <li><a class="dropdown-item" href="#">Something
-                                        else here</a></li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Action</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#"
+                                        >Another action</a
+                                    >
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#"
+                                        >Something else here</a
+                                    >
+                                </li>
                             </ul>
                         </li>
                     </ul>
                     <div class="buttonNav">
-                        <button type="button" class="btn btn-danger me-3"
-                            id="btnLog" href="login.html">Login</button>
-                        <button type="button" class="btn btn-danger"
-                            id="btnReg">Registration</button>
+                        <button
+                            type="button"
+                            class="btn btn-danger me-3"
+                            id="btnLog"
+                            href="login.html"
+                        >
+                            Login
+                        </button>
+                        <button
+                            type="button"
+                            class="btn btn-danger"
+                            id="btnReg"
+                        >
+                            Registration
+                        </button>
                     </div>
                 </div>
             </div>
         </nav>
 
-        <div class="jumbotron jumbotron-fluid text-white ">
+        <div class="jumbotron jumbotron-fluid text-white">
             <div class="container">
-                <h1 class="display-8">Film, acara TV tak terbatas, dan banyak
-                    lagi
-                    Tonton di mana pun. Batalkan kapan pun.
-                    Siap belanja ?
-
-                    Daftar dan mulai berbelanja film favorit mu !</h1>
+                <h1 class="display-8">
+                    Film, acara TV tak terbatas, dan banyak lagi Tonton di mana
+                    pun. Batalkan kapan pun. Siap belanja ? Daftar dan mulai
+                    berbelanja film favorit mu !
+                </h1>
             </div>
 
             <div class="input-group mb-3 mt-5">
-                <input type="text" class="form-control"
+                <input
+                    type="text"
+                    class="form-control"
                     placeholder="Cari judul film"
                     aria-label="Recipient's username"
-                    aria-describedby="basic-addon2">
+                    aria-describedby="basic-addon2"
+                />
                 <div class="input-group-append">
-                    <button class="btn btn-outline-secondary"
-                        type="button">Button</button>
+                    <button class="btn btn-outline-secondary" type="button">
+                        Button
+                    </button>
                 </div>
             </div>
-
         </div>
 
         <div class="img-container">
-            <div class="bg-image"
-                style="background-image: url('Dune.jpeg');"></div>
-            <div class="bg-image"
-                style="background-image: url('Dunkirk.jpeg');"></div>
-            <div class="bg-image"
-                style="background-image: url('PeakyBlinders.jpeg');"></div>
-            <div class="bg-image"
-                style="background-image: url('endgame.jpeg');"></div>
+            <div
+                class="bg-image"
+                style="background-image: url('Dune.jpeg')"
+            ></div>
+            <div
+                class="bg-image"
+                style="background-image: url('Dunkirk.jpeg')"
+            ></div>
+            <div
+                class="bg-image"
+                style="background-image: url('PeakyBlinders.jpeg')"
+            ></div>
+            <div
+                class="bg-image"
+                style="background-image: url('endgame.jpeg')"
+            ></div>
         </div>
 
         <div class="container-film shadow-lg p-3 mb-5 bg-body rounded">
@@ -108,19 +149,25 @@
             <div class="position-relative">
                 <div class="position-absolute bottom-0 end-0">
                     <nav aria-label="Page navigation example">
-                        <ul class="pagination ">
+                        <ul class="pagination">
                             <li class="page-item">
-                                <a class="page-link" href="#"
-                                    aria-label="Previous">
+                                <a
+                                    class="page-link"
+                                    href="#"
+                                    aria-label="Previous"
+                                >
                                     <span aria-hidden="true">&laquo;</span>
                                 </a>
                             </li>
-                            <li class="page-item"><a class="page-link"
-                                    href="#">1</a></li>
-                            <li class="page-item"><a class="page-link"
-                                    href="#">2</a></li>
-                            <li class="page-item"><a class="page-link"
-                                    href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">3</a>
+                            </li>
                             <li class="page-item">
                                 <a class="page-link" href="#" aria-label="Next">
                                     <span aria-hidden="true">&raquo;</span>
@@ -136,7 +183,8 @@
         <footer class="text-center text-lg-start bg-body-tertiary text-muted">
             <!-- Section: Social media -->
             <section
-                class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+                class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+            >
                 <!-- Left -->
                 <div class="me-5 d-none d-lg-block">
                     <span>Get connected with us on social networks:</span>
@@ -170,7 +218,7 @@
 
             <!-- Section: Links  -->
             <section class>
-                <div class="container text-center text-md-start mt-5 ">
+                <div class="container text-center text-md-start mt-5">
                     <!-- Grid row -->
                     <div class="row mt-3">
                         <!-- Grid column -->
@@ -181,8 +229,8 @@
                             </h6>
                             <p>
                                 Here you can use rows and columns to organize
-                                your footer content. Lorem ipsum
-                                dolor sit amet, consectetur adipisicing elit.
+                                your footer content. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.
                             </p>
                         </div>
                         <!-- Grid column -->
@@ -231,16 +279,26 @@
 
                         <!-- Grid column -->
                         <div
-                            class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4"
+                        >
                             <!-- Links -->
                             <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                            <p><i class="fas fa-home me-3"></i> Klaten, Jawa Tengah, Indonesia</p>
+                            <p>
+                                <i class="fas fa-home me-3"></i> Klaten, Jawa
+                                Tengah, Indonesia
+                            </p>
                             <p>
                                 <i class="fas fa-envelope me-3"></i>
                                 chornaeld@gmail.com
                             </p>
-                            <p><i class="fas fa-phone me-3"></i> +62-8966-4470-092</p>
-                            <p><i class="fas fa-print me-3"></i> +62-8966-4470-092</p>
+                            <p>
+                                <i class="fas fa-phone me-3"></i>
+                                +62-8966-4470-092
+                            </p>
+                            <p>
+                                <i class="fas fa-print me-3"></i>
+                                +62-8966-4470-092
+                            </p>
                         </div>
                         <!-- Grid column -->
                     </div>
@@ -250,20 +308,23 @@
             <!-- Section: Links  -->
 
             <!-- Copyright -->
-            <div class="text-center p-4"
-                style="background-color: rgba(0, 0, 0, 0.05);">
+            <div
+                class="text-center p-4"
+                style="background-color: rgba(0, 0, 0, 0.05)"
+            >
                 © 2024 Copyright:
-                <a class="text-reset fw-bold"
-                    href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                <a class="text-reset fw-bold" href="https://mdbootstrap.com/"
+                    >MDBootstrap.com</a
+                >
             </div>
             <!-- Copyright -->
         </footer>
         <!-- Footer -->
 
         <script>
-    document.getElementById("btnLog").onclick = function () {
-        location.href = "login.html";
-    };
-</script>
+            document.getElementById("btnLog").onclick = function () {
+                location.href = "login.html";
+            };
+        </script>
     </body>
 </html>
